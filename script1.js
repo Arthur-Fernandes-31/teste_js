fbce34dfdd1aa677b6c66d5d5d639426d34e5c22
Arthur <60153157097@senacrs.edu.br>
2024-10-03 09:20:24 -0300

    v2

---
+++ script1.js
@@ -0,0 +1,3 @@
+let texto = window.prompt('Digite seu nome: ');
+
+document.getElementById('titulo').innerHTML = 'Meu nome é '+texto;
--- teste-js/t1/script.js
+++
@@ -1,5 +0,0 @@
-//declaração de vraiavel
-let texto = window.prompt("Digite seu nome");
-
-//adicionar conteudo dentro de um html
-document.getElementById("titulo").innerHTML = "Meu nome é " + texto
